60ecca213481997ea22cdd4a812b0dddfd40356b
Antoine Raymond <antoine.raymond@teksial.com>
2024-12-06 11:55:57 +0100

    feat: edit content of not found template

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -7,11 +7,12 @@ export default function NotFound() {
   return (
     <div className="container py-28">
       <div className="prose max-w-none">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
+        <h1 style={{ marginBottom: 0 }}>Page introuvable</h1>
+        <p className="mb-4">La page que vous avez demandé n&apos;existe pas.</p>
       </div>
+
       <Button asChild variant="default">
-        <Link href="/">Go home</Link>
+        <Link href="/">Retournez à l&apos;accueil</Link>
       </Button>
     </div>
   )
