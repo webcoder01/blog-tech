0704f0a37dc1f8b1fb6b990c4d7ed384a2cae12a
Antoine Raymond <antoine.raymond@teksial.com>
2024-12-09 08:37:53 +0100

    refactor: redirect temporarly root path to articles

--- redirects.js
+++ redirects.js
@@ -1,20 +1,25 @@
 const redirects = async () => {
   const internetExplorerRedirect = {
-    destination: '/ie-incompatible.html',
+    destination: "/ie-incompatible.html",
     has: [
       {
-        type: 'header',
-        key: 'user-agent',
-        value: '(.*Trident.*)', // all ie browsers
+        type: "header",
+        key: "user-agent",
+        value: "(.*Trident.*)", // all ie browsers
       },
     ],
     permanent: false,
-    source: '/:path((?!ie-incompatible.html$).*)', // all pages except the incompatibility page
-  }
+    source: "/:path((?!ie-incompatible.html$).*)", // all pages except the incompatibility page
+  };
 
-  const redirects = [internetExplorerRedirect]
+  return [
+    internetExplorerRedirect,
+    {
+      source: "/",
+      destination: "/articles",
+      permanent: false,
+    },
+  ];
+};
 
-  return redirects
-}
-
-export default redirects
+export default redirects;
