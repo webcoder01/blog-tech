8c6a02384a117876288ee9f82858f3d81e53eabd
Antoine Raymond <antoine.raymond@teksial.com>
2024-12-09 09:06:15 +0100

    feat(header): remove elements and change styles

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,42 +1,37 @@
-'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+"use client";
+import { useHeaderTheme } from "@/providers/HeaderTheme";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-import type { Header } from '@/payload-types'
+import type { Header } from "@/payload-types";
 
-import { Logo } from '@/components/Logo/Logo'
-import { HeaderNav } from './Nav'
+import { HeaderNav } from "./Nav";
 
 interface HeaderClientProps {
-  header: Header
+  header: Header;
 }
 
 export const HeaderClient: React.FC<HeaderClientProps> = ({ header }) => {
   /* Storing the value in a useState to avoid hydration errors */
-  const [theme, setTheme] = useState<string | null>(null)
-  const { headerTheme, setHeaderTheme } = useHeaderTheme()
-  const pathname = usePathname()
+  const [theme, setTheme] = useState<string | null>(null);
+  const { headerTheme, setHeaderTheme } = useHeaderTheme();
+  const pathname = usePathname();
 
   useEffect(() => {
-    setHeaderTheme(null)
+    setHeaderTheme(null);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname])
+  }, [pathname]);
 
   useEffect(() => {
-    if (headerTheme && headerTheme !== theme) setTheme(headerTheme)
+    if (headerTheme && headerTheme !== theme) setTheme(headerTheme);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [headerTheme])
+  }, [headerTheme]);
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
-        <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
-        </Link>
+    <header className="py-8 bg-slate-200">
+      <div className="container flex justify-between">
         <HeaderNav header={header} />
       </div>
     </header>
-  )
-}
+  );
+};
